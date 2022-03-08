612c5e7fe0f40d575ed1a30abe116a5d3a8a4122
Christopher Kikoti <chriskikoti@gmail.com>
2022-03-08 14:02:24 +0300

    Added logic to check whether admission has pending selections/rejections to prevent processing approval

--- app/Nova/Actions/ApproveSelection.php
+++ app/Nova/Actions/ApproveSelection.php
@@ -5,11 +5,12 @@ namespace App\Nova\Actions;
 use App\Models\Admission;
 use App\Models\Student;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Fields\ActionFields;
+use Illuminate\Support\Facades\DB;
 
 class ApproveSelection extends Action {
     use InteractsWithQueue, Queueable;
@@ -24,11 +25,16 @@ class ApproveSelection extends Action {
      * @return mixed
      */
     public function handle(ActionFields $fields, Collection $models) {
+        if ($models->contains('status', 'Pending') || $models->contains('status', 'Screened')) {
+            return Action::danger('Approval denied, selection is not complete');
+        }
+
         foreach ($models as $model) {
-            $admission = Admission::firstWhere(['id' => $model->admission_id, 'locked' => 0]);
-            if ($admission != null && $model->status == 'Selected') {
+            $admission = Admission::where(['id' => $model->admission_id, 'locked' => '0'])->first();
+            if ($admission != null && $model->status == 'Selected' && $model->locked == 0) {
                 $student = new Student();
                 $student->admission_id = $model->admission_id;
+                $student->admission_application_id = $model->id;
                 $student->program_id = $admission->program_id;
                 $student->number = 'ST-' . date('Y') . '-' . $model->id;
                 $student->first_name = $model->first_name;
@@ -50,10 +56,10 @@ class ApproveSelection extends Action {
                 $student->email1 = $model->email1;
                 $student->email2 = $model->email2;
                 if ($student->save()) {
-                    $model->locked = 1;
+                    $model->locked = '1';
                     $model->save();
 
-                    $admission->locked = 1;
+                    $admission->locked = '1';
                     $admission->save();
                     return Action::message('Selections approved');
                 } else {
--- database/migrations/2022_03_04_081322_create_students_table.php
+++ database/migrations/2022_03_04_081322_create_students_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('admission_id')->nullable();
+            $table->bigInteger('admission_application_id')->nullable();
             $table->bigInteger('program_id')->nullable();
             $table->string('number', 100)->nullable();
             $table->string('first_name', 100)->nullable();
