eed5d6261c7a3d62707f037744a5b4a019db575d
Christopher Kikoti <chriskikoti@gmail.com>
2022-03-30 17:37:01 +0300

    UI amendments - TAREBI colors branding

---
+++ nova-components/Default/src/ThemeServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tarebi\Default;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::booted(function () {
+            Nova::theme(asset('/tarebi/default/theme.css'));
+        });
+
+        $this->publishes([
+            __DIR__.'/../resources/css' => public_path('tarebi/default'),
+        ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
